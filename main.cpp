53a39bfc7cd1eb6b39df2a6edfd14bf0610fc9d9
xTuTx <lion60609@gmail.com>
2018-03-24 19:00:04 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"the number is even."<<endl;
+    }
+    else
+    {
+        cout<<"the number is odd."<<endl;
+    }
+    return 0;
+}
